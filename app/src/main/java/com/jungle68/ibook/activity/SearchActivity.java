7b3f79c09c0a5fdb4de92cf7ec57671b0a453852
jungle <335891510@qq.com>
2017-06-19 23:08:11 +0800

    add search view

---
+++ app/src/main/java/com/jungle68/ibook/activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.jungle68.ibook.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jungle68.ibook.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
